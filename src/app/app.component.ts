16cb3358ca70e51f63436ca57d76f9b153446951
Arenal <ramon.arenal@tag009442251905.clients.dynatrace.org>
2018-07-10 17:21:05 +0200

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HomePage } from '../pages/home/home';
+import { Push, PushObject, PushOptions} from '@ionic-native/push';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -11,12 +13,45 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public alertCtrl:AlertController, public push: Push) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
+      this.pushsetup();
       splashScreen.hide();
     });
+  }
+
+  pushsetup() {
+    const options: PushOptions = {
+     android: {
+         senderID: '995276775605'
+     },
+     ios: {
+         alert: 'true',
+         badge: true,
+         sound: 'false'
+     },
+     windows: {}
+  };
+
+  const pushObject: PushObject = this.push.init(options);
+
+  pushObject.on('notification').subscribe((notification: any) => {
+    if (notification.additionalData.foreground) {
+      let youralert = this.alertCtrl.create({
+        title: 'New Push notification',
+        message: notification.message
+      });
+      youralert.present();
+    }
+  });
+
+  pushObject.on('registration').subscribe((registration: any) => {
+     //do whatever you want with the registration ID
+  });
+
+  pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
   }
 }
